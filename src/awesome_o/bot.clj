e6f83b54f1c6ce1ac0f9e7d98199f735f5ca1233
Jell <jean-louis@jawaninja.com>
2014-06-13 18:40:40 +0200

    Add thanks

--- src/awesome_o/bot.clj
+++ src/awesome_o/bot.clj
@@ -1,11 +1,16 @@
 (ns awesome-o.bot
   (:require [awesome-o.slack :as slack]
+            [clojure.string :as string]
             [awesome-o.adam-sandler :as movie]))
 
 (defn announcement [user-name text]
-  (slack/say "@everyone: new announcement from " user-name ":\n" text))
+  (slack/say "@EVERYONE: NEW ANNOUNCEMENT FROM " (string/upper-case user-name) ":\n" text))
+
+(defn thanks? [text]
+  (re-matches #"(?i).*(thank|thx|tack).*" text))
 
 (defn mention [user-name text]
   (cond (movie/adam-sandler? text) (slack/say (movie/generate-movie))
+        (thanks? text) (slack/say "YOU'RE WELCOME, @" (string/upper-case user-name))
         :else (slack/say "@" user-name ": I AM AWESOME-O. "
                          "YOU CAN ONLY ASK ME TO MAKE A MOVIE SCRIPT.")))
